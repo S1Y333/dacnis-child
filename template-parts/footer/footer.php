e41a9fbbba4fb0da4f70b5e632436f9f30cb617e
S1Y333 <sycici.signup@gmail.com>
2025-07-01 16:23:26 -0400

    footer update

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -24,18 +24,18 @@
 									</div>
 
 									<div class="text-right">
-										<h4 class="font-semibold mb-2 text-white">Company</h4>
+										<h4 class="font-semibold mb-2 text-white">Get in touch</h4>
 										<ul class="list-none space-y-1 text-sm p-0">
-										<li><a href="#" class="text-white no-underline hover:underline">About</a></li>
-										<li><a href="#" class="text-white no-underline hover:underline">Careers</a></li>
+										<li><a href="#" class="text-white no-underline hover:underline">Newsletter</a></li>
+										<li><a href="#" class="text-white no-underline hover:underline">Email</a></li>
 										</ul>
 									</div>
 
 									<div class="text-right">
-										<h4 class="font-semibold mb-2 text-white">Support</h4>
+										<h4 class="font-semibold mb-2 text-white">Follow us</h4>
 										<ul class="list-none space-y-1 text-sm p-0">
-										<li><a href="#" class="text-white no-underline hover:underline">Contact</a></li>
-										<li><a href="#" class="text-white no-underline hover:underline">Help Center</a></li>
+										<li><a href="#" class="text-white no-underline hover:underline">Instagram</a></li>
+										<li><a href="#" class="text-white no-underline hover:underline">Linkedin</a></li>
 										</ul>
 									</div>
 									</div>
