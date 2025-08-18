892c30a70791de0584c1a4fffe51f171f7e0f04e
S1Y333 <sycici.signup@gmail.com>
2025-08-18 13:07:46 -0400

    update footer copyright color

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -1,56 +1,61 @@
-<div  data-kubio="kubio/section" id="footer">
-	<div class="bg-gray-900 text-white pt-10">
-								<div class="container mx-auto px-4 max-w-7xl">
-									<!-- Grid layout: 5 columns total -->
-									<div class="grid grid-cols-2 md:grid-cols-6 gap-8 mb-10">
-									
-										<!-- Left side: 2 columns merged -->
-										<div class="col-span-full sm:col-span-2 md:col-span-4">
-											<img src="https://workforcepulse.ca/wp-content/uploads/2025/08/logo-wp-stacked-rev@2x.png" alt="website Logo" class=" mb-4">
+<div data-kubio="kubio/section" id="footer">
+		<div class="bg-gray-800 text-white pt-10">
+					<div class="container mx-auto px-4 max-w-7xl">
+						<!-- Grid layout: 5 columns total -->
+						<div class="grid grid-cols-2 md:grid-cols-6 gap-8 mb-10">
+						
+							<!-- Left side: 2 columns merged -->
+							<div class="col-span-full sm:col-span-2 md:col-span-4">
+								<img src="https://workforcepulse.ca/wp-content/uploads/2025/08/logo-wp-stacked-rev@2x.png" alt="website Logo" class=" mb-4">
 
-										</div>
-
-										<!-- Right side: 3 columns -->
-										<div class="text-left">
-											<h4 class="font-light mb-2 text-white">Explore</h4>
-											<div class=" bottom-0 left-0 h-0.5 bg-white w-1/4"></div>
-											<ul class="list-none space-y-1 text-sm p-0">
-												<li><a href="" class="text-white no-underline hover:underline">Home</a></li>
-												<li><a href="/about-us" class="text-white no-underline hover:underline">About</a></li>
-												<li><a href="/partners" class="text-white no-underline hover:underline">Partners</a></li>
-												<li><a href="/contact-us" class="text-white no-underline hover:underline">Dashboard</a></li>
-											</ul>
-										</div>
-
-										<div class="text-left">
-											<h4 class="font-light mb-2 text-white">Get in touch</h4>
-											<div class=" bottom-0 left-0 h-0.5 bg-white w-1/4"></div>
-											<ul class="list-none space-y-1 text-sm p-0">
-												<li><a href="#" class="text-white no-underline hover:underline">Email</a></li>
-												<li><a href="#" class="text-white no-underline hover:underline">Contact</a></li>
-											</ul>
-										</div>
-
-										
-									</div>
-																											   
-									<!-- future skills center/powered by magnet two logos-->
-									<div class="grid grid-cols-1 md:grid-cols-5 gap-8 mb-10">
-									
-											<!-- Left side: 2 columns merged -->
-										<div class="md:col-span-2 ">
-											<img src="https://workforcepulse.ca/wp-content/uploads/2025/08/logo-PBM-FSC-lockup@2x.png" alt="pbm& FSC Logo" >
-										</div>
-
-									</div>
-									<!-- Copyright -->
-									<div class="border-t border-gray-700 py-4 text-sm flex justify-content-between items-center  text-gray-400">
-										
-											<img src="https://workforcepulse.ca/wp-content/uploads/2025/08/logo-CAN-FSC-funding-1@2x.png" alt="Logo" class=" w-30 h-4 md:h-[1.8rem]">
-										
-									 		<p class="absolute right-4 text-white">&copy; <?php echo date('Y'); ?> | Privacy Policy </p>
-										
-									</div>
-								</div>
 							</div>
+
+							<!-- Right side: 3 columns -->
+							<div class="text-left">
+								<h4 class="font-light mb-2 text-white">Explore</h4>
+								<div class=" bottom-0 left-0 h-0.5 bg-white w-1/4"></div>
+								<ul class="list-none space-y-1 text-sm p-0">
+									<li><a href="" class="text-white no-underline hover:underline">Home</a></li>
+									<li><a href="/about-us" class="text-white no-underline hover:underline">About</a></li>
+									<li><a href="/partners" class="text-white no-underline hover:underline">Partners</a></li>
+									<li><a href="/contact-us" class="text-white no-underline hover:underline">Dashboard</a></li>
+								</ul>
+							</div>
+
+							<div class="text-left">
+								<h4 class="font-light mb-2 text-white">Get in touch</h4>
+								<div class=" bottom-0 left-0 h-0.5 bg-white w-1/4"></div>
+								<ul class="list-none space-y-1 text-sm p-0">
+									<li><a href="#" class="text-white no-underline hover:underline">Email</a></li>
+									<li><a href="#" class="text-white no-underline hover:underline">Contact</a></li>
+								</ul>
+							</div>
+
+							
+						</div>
+																									
+						<!-- future skills center/powered by magnet two logos-->
+						<div class="grid grid-cols-1 md:grid-cols-5 mb-4 md:gap-8 md:mb-10">
+						
+								<!-- Left side: 2 columns merged -->
+							<div class="md:col-span-2 ">
+								<img src="https://workforcepulse.ca/wp-content/uploads/2025/08/logo-PBM-FSC-lockup@2x.png" alt="pbm& FSC Logo" >
+							</div>
+
+						</div>
+						
+					</div>
+	    </div>
+		<div class="bg-black text-white">
+			<div class="container mx-auto px-4 max-w-7xl">
+				<!-- Copyright -->
+					<div class="border-t border-gray-700 py-4 text-sm flex justify-content-between items-center  text-gray-400">
+						
+							<img src="https://workforcepulse.ca/wp-content/uploads/2025/08/logo-CAN-FSC-funding-1@2x.png" alt="Logo" class=" w-30 h-4 md:h-[1.8rem]">
+						
+							<p class="absolute right-4 text-white">&copy; <?php echo date('Y'); ?> | Privacy Policy </p>
+						
+					</div>
+			</div>
+	    </div>
 </div>
